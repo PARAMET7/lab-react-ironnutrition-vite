4b7f4f6c38a24c56f0f61031169e7d9d8d508195
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-10-10 11:19:14 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+// src/App.js
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox"; // Import FoodBox
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  // Function to handle delete action
+  const handleDelete = (foodName) => {
+    setFoods(foods.filter((food) => food.name !== foodName));
+  };
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {/* Iterate over the foods array and render FoodBox for each food item */}
+      {foods.map((food) => (
+        <FoodBox
+          key={food.id} // Use unique id as key for each FoodBox
+          food={food} // Pass the food object as a prop
+          onDelete={handleDelete} // Pass the delete function
+        />
+      ))}
     </div>
   );
 }
